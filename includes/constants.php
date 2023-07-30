8e18dab15df2193a93a5bd626c619d0ba7c5b2a3
Gogs <gogs@fake.local>
2023-07-30 15:44:40 +0800

    asus_commit

--- includes/constants.php
+++ includes/constants.php
@@ -2,7 +2,7 @@
     define("DATABASE", "mariphil");
     define("USERNAME", "root");
     define("PASSWORD", "");
-    define("PORT", "3308");
+    define("PORT", "3306");
     define("HOST", "localhost");
-    define("SERVER", "localhost:3308");
+    define("SERVER", "localhost:3306");
 ?>
