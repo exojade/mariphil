22d82b06f4f0fe3fd77275e4f5dd50c6a35b1ad4
Gogs <gogs@fake.local>
2023-08-06 18:51:41 +0800

    asus_commit

--- public/dashboard_system/dashboard_scholar.php
+++ public/dashboard_system/dashboard_scholar.php
@@ -71,7 +71,7 @@
               </div>
               <!-- /.card-body -->
               <div class="card-footer">
-                Footer
+              
               </div>
               <!-- /.card-footer-->
             </div>
